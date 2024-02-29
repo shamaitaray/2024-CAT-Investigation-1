6e5c8f89535c251c20933c3fde47edccef6d3243
Shamaita Ray <shamaita.ray@student.education.wa.edu.au>
2024-02-29 11:26:03 +0800

    Dear Diary, It's been a while since my last commit. Sorry about that, I merely realised how badly I didn't want to do this assignment.
    Anyway, this update is to say that I have made a compound interest calculator! Woohoo!
    I made a function that calculates compound interest which you just need to put input
    into. The thing is, I don't know whether my time conversion thing will work, so the
    compounding periods are all custom at the moment, rather than being yearly, quarterly,
    etc. So the next thing I'll do is that :')
    
    I just realised that no one is going to keep money in a bank for three months if
    the interest only compounds a year, right? So technically, I don't have to worry about
    converting from weeks to quarter, or some thing like that.
    
    ...right?
    
    I don't have the intellect to come up with something for that, so I'm just going to hope
    that what I just said is right.
    
    Now that I think of it, I should probably be asking a teacher instead of telling my
    "diary" this.
    
    Eh. Whatever.
    
    But yeah, I managed to do compound interest as well! Who knows, maybe I will be able to
    finish this assignment on time (ahhahha. touch wood).
    
    Bye!

--- part1siandci.py
+++ part1siandci.py
@@ -2,15 +2,31 @@ def simple_interest(P, R, T):
     si = P*(R/100)*T
     return round(si, 2)
 
+def compound_interest(P, R, cT, T):
+    ci = P*((1+((R/100)/cT))**(cT*T))
+    return round(ci, 2)
+
 print("MODULE 1: SIMPLE AND COMPOUND INTEREST COMPARISON")
 print("") 
+
 print("Simple Interest Account:")
+SP = float(input("Enter the principal amount in $: "))
+SR = float(input("Enter the interest rate (enter 7% as 7):"))
+print("")
 
-P = float(input("Enter the principal amount in $: "))
-R = float(input("Enter the interest rate (enter 7% as 7):"))
-T = float(input("Enter the amount of time to project into the future:"))
+print("Compound Interest Account:")
+CP = float(input("Enter the principal amount in $: "))
+CR = float(input("Enter the interest rate (enter 7% as 7):"))
+CT = float(input("Enter the compounding period(year, quarter, month, week, day, custom): "))
+print("")
 
-print(f"SI account: P = {P}, r = {R}% per year")
-print(f"Projection timeframe: {T} year")
+PT = float(input("Enter the amount of time to project into the future:"))
+print("")
 
-print(f"SI Account projected amount: ${P + simple_interest(P, R, T)}, Interest earned: ${simple_interest(P, R, T)}")
+print(f"SI account: P = {SP}, r = {SR}% per year")
+print(f"CI account: P = {CP}, r = {CR}% per year")
+print(f"Projection timeframe: {PT} year")
+print("")
+
+print(f"SI Account projected amount: ${SP + simple_interest(SP, SR, PT)}, Interest earned: ${simple_interest(SP, SR, PT)}")
+print(f"CI Account projected amount: ${CP + compound_interest(CP, CR, CT, PT)}, Interest earned: ${compound_interest(CP, CR, CT, PT)}")
