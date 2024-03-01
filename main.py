fee1926e85b6fceb2b999cfb8f3b659a76dbe04d
Shamaita Ray <shamaita.ray@student.education.wa.edu.au>
2024-03-01 21:49:06 +0800

    Dear Diary, I finished Part 1 of the investigation. I had actually doen that yesterday in class, but I didn't have enough time to commit then. I only just opened my laptop (for purposes at least), hence the late commit. Sorry about that.
    So what I did was...
    
    Let me think this was a whole DAY ago.
    
    Ok so what I was having trouble with was the compounding frequency nonsense, and the
    conversion of time units. I made an input that asks for the compounding frequency, then
    converts it into a number that fits however many time it compounds per the interest rate
    time unit. Later(today), I also added an if/else statement so if you decide to have a
    custom compounding period its asks you what it is and uses that instead of a time unit.
    If that makes sense.
    
    I also had to do the rest of the time conversion stuff so now there are calculations that
    assure that everything is the same time unit, which is basically just putting input through
    my converter and making it all the same unit.
    
    I'm pretty sure that's it.
    
    I think I'll immediately start working on the next because I have a hunch that I won't finish
    this thing on time otherwise. T^T

--- main.py
+++ main.py
@@ -1,33 +1,70 @@
 #2024 CAT Investigation 1#
 
-
 conversions = {
     'year' : {
+        'year': 1,
         'quarter': 4,
         'month': 12,
         'week': 52,
         'day': 365,
     },
-
     'quarter' : {
+        'quarter': 1,
         'month': 3,
         'week': 12,
         'day': 90,
     },
-    
     'month' : {
+        'month': 1,
         'week': 4,
         'day': 30,
     },
-
     'week' : {
+        'week': 1,
         'day': 7,
     },
 }
 
-time = input("unit of time: ")
-many = int(input('how many? '))
-time2 = input('converting to: ')
+def simple_interest(P, R, T):
+    si = P*(R/100)*T
+    return round(si, 2)
 
+def compound_interest(P, R, cT, T):
+    ci = P*((1+((R/100)/cT))**(cT*T))
+    return round(ci, 2)
 
-print(many*conversions[time][time2])
+print("MODULE 1: SIMPLE AND COMPOUND INTEREST COMPARISON")
+print("") 
+
+print("Simple Interest Account:")
+SP = float(input("Enter the principal amount in $: "))
+SR = float(input("Enter the interest rate (enter 7% as 7): "))
+SU = input('Enter the interest rate time unit (year, quarter, month, week, day: ')
+print("")
+
+print("Compound Interest Account:")
+CP = float(input("Enter the principal amount in $: "))
+CR = float(input("Enter the interest rate (enter 7% as 7): "))
+CU = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
+CPU = input("Enter the compounding period(year, quarter, month, week, day, custom): ")
+if CPU == "custom":
+    cT = int(input("Enter the number of compounding periods per interest rate time unit: "))
+    CPU = cT
+else:
+    cT = conversions[CU][CPU]
+print("")
+
+PT = float(input("Enter the amount of time to project into the future: "))
+PU = input("Enter the projection time unit (year, quarter, month, week, day): ")
+print("")
+
+print(f"SI account: P = {SP}, r = {SR}% per {SU}")
+print(f"CI account: P = {CP}, r = {CR}% per {CU}, Compounding frequency: {CPU}")
+print(f"Projection timeframe: {PT} {PU}")
+print("")
+
+ST = PT * conversions[PU][SU]
+CT = PT * conversions[PU][CU]
+
+print(f"SI Account projected amount: ${SP + simple_interest(SP, SR, ST)}, Interest earned: ${simple_interest(SP, SR, PT)}")
+print(f"CI Account projected amount: ${CP + compound_interest(CP, CR, cT, CT)}, Interest earned: ${compound_interest(CP, CR, cT, PT)}")
