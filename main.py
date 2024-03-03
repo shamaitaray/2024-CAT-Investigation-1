7b5ec70b8ce0efcd42748ce7018cd69394a85b64
Shamaita Ray <shamaita.ray@student.education.wa.edu.au>
2024-03-03 22:02:11 +0800

    Dear Diary,
    Oh my FLIPPING GOD, you will not believe what I went through.
    
    I LITERALLY went through all 7 stages of grief trying to do Part 2 of this investigation.
    I might actually need to attend therapy to get over the trauma of what I just experienced.
    And I'm not joking, either.
    
    So it started off fine. I searched up how to calculate the time factor, so then I could use
    it in my function.
    
    "Wait... what does 'ln' mean?"
    
    It was natural log. I had to import numpy and use fricking logarithms.
    
    "O-ok! This isn't too bad... There! I have my function!"
    "Now I just need to make gather input, and plug it into the function! Easy peasy, right?"
    
    It seemed simple emough. I used the structure from the last bit of code to make mine, and so
    far, everything was fine.
    
    Then this crap happened:
    Time taken: 0.017713837964709055 quarter
    
    .
    
    Um, EXCUSE me? HOW CAN YOU EVEN SAVE FOR 0.017713837964709055 QUARTERS?
    
    I checked the function. It was perfectly fine.
    
    "Well maybe it's the input!"
    
    I checked the input. It was also fine.
    
    I went back to the website and learnt about how they solved for time, in hopes of fixing the
    function.
    
    I altered it slightly. IT STILL OUTPUTTED 0.017713837964709055.
    
    I got my dad to help me.
    
    "Dad- what- what is this monster? I-I did everything right- w-why is it outputting 0.017713837964709055?"
    
    We went through and solved it from the second last step. My calculator outputted it just fine.
    (Note to self: Never ever rely on these programs in times of need. Calculators are better.)
    
    So then I thought, "Why don't I split the calculation into two parts? Maybe then it'll output the
    right thing!"
    
    Time taken: 0.017713837964709055 quarter
    
    It is in absolute honesty that I say I was on the verge of tears. My dad then suggested, "Why
    don't you break the function down into each step we did, then run it in a separate file and
    return each individual step?"
    
    "sniff... i-it's worth a shot."
    
    So then I opened a new file, copied the function into there, and returned each step of that
    unholy equation.
    
    The thing is, the first two steps were incorrect(at least according to my calcuator), but the
    last two and output were correct now?
    
    "Well, at least it's not 0.017713837964709055 anymore."
    - Uchchhal Ray and Shamaita Ray, 2024
    
    So I went and put my new function into the main part of the code, and then I realised the the
    stupid rounding function rounds to the closest integer. We needed it to round up, no matter what.
    
    So THEN I went and searched that up and after a lengthy explanation of things I didn't need to know,
    I discovered I needed to use the math module.
    
    I had been using numpy before.
    
    So I got rid of numpy, imported math, changed the logarithm function to math.log, rounded the result
    of the equation up, and with bated breath, ran the program...
    
    Time taken: 9 quarter
    
    .
    
    ..
    
    ...
    
    LET'S
    
    FLIPPING
    
    GOOOOOOOOO
    
    That was the biggest dopamine boost I've had in years, I can tell you that.
    
    Then I realised I had to commit and in extension, relive my trauma.
    
    So I'm still as depressed as I was at the beginning of this, if not more.
    
    I'm not doing anymore of this investiagtion tonight; I might actually go insane if I do.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# --- 2024 CAT Investigation 1 --- #
+### --- 2024 CAT Investigation 1 --- ###
 
+#importing math to use the logarithm function when calulcating the time taken to reach a target
+#also to do rounding up because the round function doesn't do that, apparently
+import math
 #functions:
 conversions = {
+
     'year' : {
         'year': 1,
         'quarter': 4,
@@ -25,18 +29,27 @@ conversions = {
         'day': 7,
     },
 }
-
+#this is for conversions of time periods
 def simple_interest(P, R, T):
     si = P*(R/100)*T
     return round(si, 2)
-
-def compound_interest(P, R, cT, T):
+#this calcuates simple interest
+def compound_interest(P, r, n, A):
     ci = P*((1+((R/100)/cT))**(cT*T))
     return round(ci, 2)
+#this calculates compound interest
+def time_target(P, r, n, A):
+    r = r/100
+    a = math.log(A/P)
+    b = math.log(1 + (r/n))
+    nt = a/b
+    time = nt/n
+    return math.ceil(time)
+#this calculates the time needed to reach the target
 
-
-#imformation for user + menu
-print("""WELCOME TO, UH, WHATEVER THIS IS!!!
+#information for user + menu
+print("""***
+WELCOME TO, UH, WHATEVER THIS IS!!!
 This program has five modules. Choose a module by typing its number:
 (1) Compare simple and compound interest savings acccounts
 (2) Calculate the time for a CI savings account to reach a target amount
@@ -44,44 +57,74 @@ This program has five modules. Choose a module by typing its number:
 (4) Model a CI savings account with regular deposits
 (5) Model increases in compounding frequency""")
 
-menu = input("Enter 1 to 5, or 6 to quit: ") 
+while True:  #use of while loop means the program can be used again and again wihtout rerunning
+    print("***")
 
-print("***")
+    menu = input("Enter 1 to 5, or 6 to quit: ") 
 
-if menu == '1':
-    print("MODULE 1: SIMPLE AND COMPOUND INTEREST COMPARISON")
-    print("") 
-
-    print("Simple Interest Account:")
-    SP = float(input("Enter the principal amount in $: "))
-    SR = float(input("Enter the interest rate (enter 7% as 7): "))
-    SU = input('Enter the interest rate time unit (year, quarter, month, week, day: ')
     print("")
+    
+    if menu == "6":
+        print("Ok. Stopping program...")
+        break
 
-    print("Compound Interest Account:")
-    CP = float(input("Enter the principal amount in $: "))
-    CR = float(input("Enter the interest rate (enter 7% as 7): "))
-    CU = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
-    CPU = input("Enter the compounding period(year, quarter, month, week, day, custom): ")
-    if CPU == "custom":
-        cT = int(input("Enter the number of compounding periods per interest rate time unit: "))
-        CPU = cT
-    else:
-        cT = conversions[CU][CPU]
-    print("")
+    elif menu == '1':
+        print("MODULE 1: SIMPLE AND COMPOUND INTEREST COMPARISON")
+        print("") 
 
-    PT = float(input("Enter the amount of time to project into the future: "))
-    PU = input("Enter the projection time unit (year, quarter, month, week, day): ")
-    print("")
+        print("Simple Interest Account:")
+        SP = float(input("Enter the principal amount in $: "))
+        SR = float(input("Enter the interest rate (enter 7% as 7): "))
+        SU = input('Enter the interest rate time unit (year, quarter, month, week, day: ')
+        print("")
 
-    print(f"SI account: P = {SP}, r = {SR}% per {SU}")
-    print(f"CI account: P = {CP}, r = {CR}% per {CU}, Compounding frequency: {CPU}")
-    print(f"Projection timeframe: {PT} {PU}")
-    print("")
+        print("Compound Interest Account:")
+        CP = float(input("Enter the principal amount in $: "))
+        CR = float(input("Enter the interest rate (enter 7% as 7): "))
+        CU = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
+        CPU = input("Enter the compounding period(year, quarter, month, week, day, custom): ")
+        if CPU == "custom":
+            cT = int(input("Enter the number of compounding periods per interest rate time unit: "))
+            CPU = cT
+        else:
+            cT = conversions[CU][CPU]
+        print("")
 
-    ST = PT * conversions[PU][SU]
-    CT = PT * conversions[PU][CU]
+        PT = float(input("Enter the amount of time to project into the future: "))
+        PU = input("Enter the projection time unit (year, quarter, month, week, day): ")
+        print("")
 
-    print(f"SI Account projected amount: ${SP + simple_interest(SP, SR, ST)}, Interest earned: ${simple_interest(SP, SR, PT)}")
-    print(f"CI Account projected amount: ${CP + compound_interest(CP, CR, cT, CT)}, Interest earned: ${compound_interest(CP, CR, cT, PT)}")
+        print(f"SI account: P = {SP}, r = {SR}% per {SU}")
+        print(f"CI account: P = {CP}, r = {CR}% per {CU}, Compounding frequency: {CPU}")
+        print(f"Projection timeframe: {PT} {PU}")
+        print("")
+
+        ST = PT * conversions[PU][SU]
+        CT = PT * conversions[PU][CU]
+
+        print(f"SI Account projected amount: ${SP + simple_interest(SP, SR, ST)}, Interest earned: ${simple_interest(SP, SR, PT)}")
+        print(f"CI Account projected amount: ${CP + compound_interest(CP, CR, cT, CT)}, Interest earned: ${compound_interest(CP, CR, cT, PT)}")
+
+    elif menu == '2':
+        print("MODULE 2: TIME FOR A CI ACCOUNT TO REACH A TARGET AMOUNT")
+        P = float(input("Enter the principal amount in $: "))
+        r = float(input("Enter the interest rate (enter 7% as 7): "))
+        CU = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
+        CPU = input("Enter the compounding period(year, quarter, month, week, day, custom): ")
+        if CPU == "custom":
+            n = float(input("Enter the number of compounding periods per interest rate time unit: "))
+            CPU = cT
+        else:
+            n = conversions[CU][CPU]
+        print("")
+
+        A = float(input("Enter target amount: "))
+        print("")
+        
+        print(f"CI account: P = {P}, r = {r}% per {CU}, Compounding frequency: {CPU}")
+        print(f"Target amount: ${A}")
+        print("")
+
+        print(f"Time taken: {time_target(P, r, n, A)} {CPU}")
+
 
--- part1siandci.py
+++ part1siandci.py
@@ -1,32 +1,11 @@
-def simple_interest(P, R, T):
-    si = P*(R/100)*T
-    return round(si, 2)
+import numpy
 
-def compound_interest(P, R, cT, T):
-    ci = P*((1+((R/100)/cT))**(cT*T))
-    return round(ci, 2)
+def time_target(P, r, n, A):
+    r = r/100
+    a = numpy.log(A/P)
+    b = numpy.log(1 + (r/n))
+    nt = a/b
+    time = nt/n
+    return a, b, nt, time
 
-print("MODULE 1: SIMPLE AND COMPOUND INTEREST COMPARISON")
-print("") 
-
-print("Simple Interest Account:")
-SP = float(input("Enter the principal amount in $: "))
-SR = float(input("Enter the interest rate (enter 7% as 7):"))
-print("")
-
-print("Compound Interest Account:")
-CP = float(input("Enter the principal amount in $: "))
-CR = float(input("Enter the interest rate (enter 7% as 7):"))
-CT = float(input("Enter the compounding period(year, quarter, month, week, day, custom): "))
-print("")
-
-PT = float(input("Enter the amount of time to project into the future:"))
-print("")
-
-print(f"SI account: P = {SP}, r = {SR}% per year")
-print(f"CI account: P = {CP}, r = {CR}% per year")
-print(f"Projection timeframe: {PT} year")
-print("")
-
-print(f"SI Account projected amount: ${SP + simple_interest(SP, SR, PT)}, Interest earned: ${simple_interest(SP, SR, PT)}")
-print(f"CI Account projected amount: ${CP + compound_interest(CP, CR, CT, PT)}, Interest earned: ${compound_interest(CP, CR, CT, PT)}")
+print(time_target(1000, 5, 4, 1500))
